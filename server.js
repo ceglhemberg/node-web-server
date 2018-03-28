31f74ad3779e84c8177ca0b67e2f6b18735c5181
Erik Hemberg <erikhemberg@Eriks-iMac.local>
2018-03-28 23:03:46 +0200

    Add startscript and heroku port

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+  let now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
   fs.appendFile('server.log', log + '\n');
@@ -50,6 +51,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
